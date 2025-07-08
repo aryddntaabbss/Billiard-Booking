6f531f00a5033bfa1a24ed4d0dfa21e6d0b608dd
aryaddinata <aryaddinataabbas017@gmail.com>
2025-07-08 19:22:29 +0900

    #003 Integrasi Google OAuth (Socialite)

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class AuthController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        $googleUser = Socialite::driver('google')->user();
+
+        $user = User::firstOrCreate(
+            ['email' => $googleUser->email],
+            [
+                'name' => $googleUser->name,
+                'google_id' => $googleUser->id,
+            ]
+        );
+
+        Auth::login($user);
+
+        return redirect('/')->with('success', 'Login berhasil');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/auth/redirect', [AuthController::class, 'redirectToGoogle'])->name('google.redirect');
+Route::get('/auth/callback', [AuthController::class, 'handleGoogleCallback'])->name('google.callback');
+
 
 Route::get('/', function () {
     return view('welcome');
